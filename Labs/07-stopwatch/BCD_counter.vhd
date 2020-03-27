2d4737a3702ded7920ada760988a13824e4b9026
SimonaSadlekova <58397657+SimonaSadlekova@users.noreply.github.com>
2020-03-27 12:28:23 +0100

    Update BCD_counter.vhd

--- Labs/07-stopwatch/BCD_counter.vhd
+++ Labs/07-stopwatch/BCD_counter.vhd
@@ -7,7 +7,7 @@ use ieee.std_logic_unsigned.all;    -- Provides unsigned numerical computation
 ------------------------------------------------------------------------
 entity BCD_cnt is
 generic (
-    nax_val : positive := 9      -- Number of bits
+    max_val : positive := 9      -- Number of bits
 );
 port (
     clk_i    : in  std_logic;
